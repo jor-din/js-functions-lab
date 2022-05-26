4557eb4aaa8dfeed71f1bfe130caa3369e2a19ef
jordinhuang <jordinhuang@gmail.com>
2022-05-25 23:37:10 -0400

    complete exercise 7

--- app.js
+++ app.js
@@ -96,6 +96,10 @@ Exercise 6:
   - Define a function, as a function expression, `numArgs` that returns the number of arguments passed to the function when called.
 */
 
+let numArgs = function(numbers) {
+
+}
+
 
 
 console.log(
@@ -111,6 +115,9 @@ console.log(
 Exercise 7:
   - Define a function, as a function declaration, `reverseString` that takes a string, reverses the characters, and returns it. For example, `reverseString('rockstar');` would return the string "ratskcor".
 */
+function reverseString(string) {
+  return string.split('').reverse().join('')
+}
 
 
 
